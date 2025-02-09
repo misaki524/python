59d3e06bec4247e51219df98bcaaec106797c19c
kiyo1117coda <kiyo1117coda@docomo.ne.jp>
2025-02-10 08:01:59 +0900

    create_計算

--- test_01.py
+++ test_01.py
@@ -5,7 +5,18 @@ high = 100
 print(low,high)
 
 for i in range(7):
+#lowとhightが同じならループを抜ける
+  if low == high:
+    break
+
 #コンピューターの推測値を確認
   guess = (low + high)//2
   print('あなたの数字は',guess,'より大きいですか？(yes/no)')
   answer = input()
+  #ユーザーの答えにより分岐
+  if answer == "yes":
+    low = guess + 1
+  else:
+    high = guess
+
+print('あなたの考えている数字は',low,'ですね')
