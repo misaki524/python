0ff8e2255ba06026bee99748133051ecbc08f16c
kiyo1117coda <kiyo1117coda@docomo.ne.jp>
2025-02-19 23:00:16 +0900

    #コメント

--- kintai/main.py
+++ kintai/main.py
@@ -1,10 +1,10 @@
-from rumps import *
-from record import *
+from rumps import * #MacOSのメニューバーアプリケーションを作成するためのライブラリ
+from record import * #record.pyの呼び出し
 import download,datetime,makeworklist
 
-class RumpsTest(App):
-    def __init__(self):
-        super(RumpsTest,self).__init__("kintai",icon="images/PC.png",title=None,quit_button="kintaiの終了")
+class RumpsTest(App): #RumpsTesttというクラスを作成してその中に記載
+    def __init__(self): #def__int__(self)の中でアプリ全体の設定
+        super(RumpsTest,self).__init__("kintai",icon="images/PC.png",title=None,quit_button="kintaiの終了") #super()は親クラス。selfはインタンス(オブジェクト)
         self.menu=[
           MenuItem("開始",callback=self.start),
           MenuItem("終了"),
@@ -15,31 +15,50 @@ class RumpsTest(App):
           None,
           MenuItem("環境設定"),
           MenuItem("詳細")
-        ]
+        ] #menu属性にMenuItem型で項目をリスト形式に記載
         self.menu["開始"].add(MenuItem("開始1"))
         self.menu["開始"].add(MenuItem("開始2"))
-        self.menu["kintaiの出力"].add(MenuItem("今月",callback=self.make_work_list))
+        self.menu["kintaiの出力"].add(MenuItem("今月",callback=self.make_work_list)) #kintaiの出力を押出た時、make_work_listをcallbackで呼び出し実行。今月をmenuにadd関数で項目を追加
         self.menu["kintaiの出力"].add(MenuItem("先月",callback=self.make_last_work_list))
 
 
-    def start(self,sender):
-        print("開始")
+    def start(self,sender): #record.pyを呼び出す
+        #記録開始
+        record("開始","") #開始という文字列を空の文字列を引数として渡す
+        global my_timer,count #my_timer,countは引数　#グローバル変数（どこからでもアクセスできる変数)
+        count=0 #countを0に初期化
+        my_timer= Timer(self.pass_time,1) #Timerクラスを処理１秒間ごとに処理をself.pass_timeに設定
+        my_timer.start() #Timerを実際にスタートする
+        #メニューの更新
+        self.menu["開始"].set_callback(None) #set_callback関数で呼び出し。開始を押しても何も起こらない
+        self.menu["終了"].set_callback(self.end) #set_callback関数で呼び出し。終了を押したらself.endメゾットを呼び出す
+        self.menu["取り消し"].set_callback(self.cancel) #et_callback関数で呼び出し。取り消しを押したらself.cancelメゾットを呼び出す
+        #アイコンが変更する
         self.icon="images/hamster.png"
-        global my_timer,count
-        count=0
-        my_timer= Timer(self.pass_time,1)
-        self.menu["開始"].set_callback(None)
-        self.menu["開始"].title="仕事中"
-        if alert("取り消しますか？",ok="はい",cancel="いいえ"):
-            print("開始")
-        else:
-            print("いいえが押されました")
 
     def pass_time(self,sender):
         global count
-        print(count)
-        count += 1
+        #カウントを1を進める
+        count += 1 #countが0を1ずつ増やす
 
+        #時間の表示形式を変更
+        pass_time=datetime.timedelta(seconds=int(cont)+1) #countが10だとtimedeltaが0:00:00に変換。countが75だとtimedeltaが0:01:15に変換
+
+        #経過時間項目の横に時間を表示する
+        self.menu["経過時間"].title="経過時間："+str(pass_time) #経過時間を文字列で表示
+
+    def cancel(self,sender):
+        if alert("取り消しますか？",ok="はい",cancel="いいえ"): #alert関数でメッセージを表示をする。OK＝TRUE、FALSE＝いいえを返す。TRUEの場合下記を処理
+            #取り消しの記録
+            record("取り消し","") #取り消しという文字列を空の文字列を引数として渡す
+            #タイマーの停止
+            my_timer.stop()
+            #表示項目の更新
+            self.menu["終了"].set_callback(None)#set_callback関数で呼び出し。終了を押しても何も起こらない
+            self.menu["開始"].set_callback(self.start) #set_callback関数で呼び出し。開始を押したらself.endメゾットを呼び出す
+            self.menu["取り消し"].set_callback(None) #set_callback関数で呼び出し。終了を押しても何も起こらない
+            self.menu["経過時間"].title="経過時間" #経過時間に戻して経過時間の表示のリセット
+            self.icon="images/hamster.png"
 
     def end(self,sender):
         print("終了")
@@ -49,3 +68,7 @@ class RumpsTest(App):
 if __name__=="__main__":
     app=RumpsTest()
     app.run()
+
+
+#補足：
+#defは関数を定義するもの
